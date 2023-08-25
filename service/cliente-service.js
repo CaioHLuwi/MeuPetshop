6d786c4f3a1c76dcf131326c8e9d0b0a6c766469
CaioHLuwi <caiohenriquelw2004@gmail.com>
2023-08-25 16:50:43 -0300

    Substituindo a promisse pela Fetch API

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -17,23 +17,11 @@ const criaNovaLinha = (nome, email) => {
 const tabela = document.querySelector('[data-tabela]') // Elemento pai
 
 const listaClientes = () => {
-    const promise = new Promise((resolve, reject) => {
-        const http = new XMLHttpRequest()
-        http.open('GET', 'http://localhost:3000/profile')
-        // 'O que eu quero falar com o servidor', 'endereço para onde vou enviar a requisição' 
-
-        http.onload = () => {
-            if(http.status >= 400){ // 400 é a badRequest
-                reject(JSON.parse(http.response)) 
-            } else {
-                resolve(JSON.parse(http.response))
-            }
-        }
-        // Ao carregar vai imprimir o que o servidor respondeu
-        http.send()
-        })
-    console.log(promise)
-    return promise
+    return fetch(`http://localhost:3000/profile`)
+    .then( resposta => {
+        return resposta.json
+    })
+    // A Fetch API já faz um GET e devolve uma promisse por padrão.
 }
 
 listaClientes()
