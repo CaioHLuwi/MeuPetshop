3c51cd9fb1fa7e720f1afff6d67e97626837323e
CaioHLuwi <caiohenriquelw2004@gmail.com>
2023-08-28 14:07:51 -0300

    Implementação: Função para criar cliente

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -6,7 +6,24 @@ const listaClientes = () => {
     // A Fetch API já faz um GET e devolve uma promisse por padrão.
 }
 
+const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({ // Retorna como texto
+            nome: nome, // Parametro da função nome
+            email: email // Parametro da função nome
+        })
+    })
+    .then( resposta => {
+        return resposta.body
+    })
+}
+
 export const clienteService = {
-    listaClientes
+    listaClientes,
+    criaCliente
 }
 
