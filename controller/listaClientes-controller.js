f3a07ca1a88412aef33a0bf62ae170f57de6cb99
CaioHLuwi <caiohenriquelw2004@gmail.com>
2023-09-04 10:59:58 -0300

    Refatorando usando Async/Await

--- controller/listaClientes-controller.js
+++ controller/listaClientes-controller.js
@@ -19,21 +19,21 @@ const criaNovaLinha = (nome, email, id) => {
 
 const tabela = document.querySelector('[data-tabela]') // Elemento pai
 
-tabela.addEventListener('click', (evento) => {
+tabela.addEventListener('click', async (evento) => {
     let ehBotaoDeletar = evento.target.className == 'botao-simples botao-simples--excluir'
     if(ehBotaoDeletar){
         const linhaCliente = evento.target.closest('[data-id]') // Encontra o elemento mais próximo ao especificado
         let id = linhaCliente.dataset.id
-        clienteService.removeCliente(id)
-        .then( () => {
-            linhaCliente.remove()
-        })
+        await clienteService.removeCliente(id)
+        linhaCliente.remove()
     }
 })
 
-clienteService.listaClientes()
-.then( data => {
-        data.forEach(elemento => {
-            tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email, elemento.id))
-        })
-})
+const render = async () => {
+    const clienteService = await clienteService.listaClientes()
+    clienteService.forEach(elemento => {
+        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email, elemento.id))
+    })
+}
+render()
+
